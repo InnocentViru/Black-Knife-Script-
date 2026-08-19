620be06d2edc8b73da246874b6f53c6afcb59958
VirusSX <fcharlene02@gmail.com>
2026-08-18 21:42:08 -0300

    Add ScriptExecute.lua to load external script

---
+++ ScriptExecute.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InnocentViru/Black-Knife-Script-/refs/heads/main/Script.lua?token=GHSAT0AAAAAAEBYWB7NOOIRFZ4EQBHXC7S42UE7PSA?t=" .. tick()))()
